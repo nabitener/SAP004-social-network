4645804f9f9f86fa35ee47f12a957a063682c8ec
nabitener <na_bitener@hotmail.com>
2020-06-04 17:25:03 -0300

    Refatoração css, nova paleta de cores, autenticação email e senha

---
+++ public/pages/login/data.js
@@ -0,0 +1,9 @@
+export const authEmailAndPassword = (email, password) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+};
--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -1,27 +1,31 @@
-// Aqui serão criados os eventos de Manipulação de DOM e templates
+import { authEmailAndPassword } from "./data.js";
 
 export const login = () => {
-const container = document.createElement("div");
-container.classList.add("div-container");
+  const container = document.createElement("div");
+  container.classList.add("div-container");
 
-container.innerHTML = `
-<div class="img-login" class="login">
-<img src="imagens/logo.jpg" alt="Img Logo" width=100% height=130%>
+  container.innerHTML = `
+<div class="img-login class="login">
+<img src="imagens/logo.jpg" alt="Img Logo" width=100% height=110%>
 </div>
-<form class="form" class="login">
-<p class="logo"><h2>Nome do app</h2></p> 
-<input type="email" id="email" class="login-email" class="input-login" placeholder="E-mail">
-<input type="password" id="pwd" class="login-pwd" class="input-login" placeholder="Password">
+<form class="form login">
+<p class="logo text-p">Nome do app</p> 
+<input type="email" id="email" class="login-email input-login" placeholder="E-mail" required>
+<input type="password" id="pwd" class="login-pwd input-login" placeholder="Password" required>
 <br>
 <button id="login" class="button-login">Log in</button>
-<p class="other">Ou entre com...</p>
+<p class="other text-p">Ou entre com...</p>
 <input type="image" id="btn-google" class="btn-google" src="google.png">
-<p class="register">Não tem uma conta? <a href="/#record">Cadastre-se</a></p>
+<p class="register text-p">Não tem uma conta? <a href="/#record">Cadastre-se</a></p>
 </form>
-`
-return container;
-};
+`;
+  const botaoLogin = container.querySelector("#login");
+  const inputEmail = container.querySelector("#email");
+  const inputSenha = container.querySelector("#pwd");
 
-const botaoLogin = document.querySelector("#login");
-const inputEmail = document.querySelector("#email");
-const inputSenha = document.querySelector("#pwd");
+  botaoLogin.addEventListener("click", (event) => {
+    event.preventDefault();
+    authEmailAndPassword(inputEmail, inputSenha);
+  });
+  return container;
+};
