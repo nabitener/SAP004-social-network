972224e0dd60e5d3698d601dc154c726a7f5eae4
Aline <alinesanqueza@gmail.com>
2020-06-08 17:46:25 -0300

    autenticação funcionando no firebase

--- public/pages/login/data.js
+++ public/pages/login/data.js
@@ -1,27 +1,31 @@
 export const welcome = email => `Bem-vindo ${email}! Que bom ver você aqui!`;
 export const errorMessage = message => `Falha: ${message}`;
 export const authEmailAndPassword = (email, password) => {
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email.value, password.value)
-    .then(() => {
-      alert(`${welcome(email.value)}`);
-    })
-    .catch((error) => {
-      alert(`${errorMessage(error.message)}`);
-    });
+	firebase
+	.auth()
+	.signInWithEmailAndPassword(email.value, password.value)
+	.then(() => {
+		alert(`Bem-vindo ${email.value}`);
+	})
+	.catch((error) => {
+		alert(`Falha: ${error.message}`);
+	});
 };
 export const signIn = (provider) => {
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      console.log(result);
-      const token = result.credential.accessToken;
-      alert(`${welcome(inputEmail.value)}`);
-    })
-    .catch((error) => {
-      console.log(error);
-      alert(`${errorMessage(error.message)}`);
-    });
-  };
+	firebase
+	.auth()
+	.signInWithPopup(provider)
+	.then((result) => {
+		console.log(result);
+		/*const token = result.credential.accessToken;*/
+		alert(`Bem-vindo`);
+	})
+	.catch((error) => {
+		console.log(error);
+		alert(`Falha: ${error.message}`);
+	});
+	};
+
+
+
+
--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -6,17 +6,17 @@ export const login = () => {
 
   container.innerHTML = `
 <div class='img-login login'>
-<img src='imagens/logo.jpg' alt='Img Logo' width=100% height=110%>
+  <img src='imagens/logo.jpg' alt='Img Logo' width=100% height=110%>
 </div>
 <form class='form login'>
-<p class='logo text-p'>Nome do app</p> 
-<input type='email' id='email' class='login-email input-login' placeholder='E-mail' required>
-<input type='password' id='pwd' class='login-pwd input-login' placeholder='Password' required>
-<br>
-<button id='login' class='button-login'>Log in</button>
-<p class='other text-p'>Ou entre com...</p>
-<input type='image' id='btn-google' class='btn-google' src='google.png'>
-<p class='register text-p'>Não tem uma conta? <a href='/#record'>Cadastre-se</a></p>
+  <p class='logo text-p'>Nome do app</p> 
+  <input type='email' id='email' class='login-email input-login' placeholder='E-mail' required>
+  <input type='password' id='pwd' class='login-pwd input-login' placeholder='Password' required>
+  <br>
+  <button id='login' class='button-login'>Log in</button>
+  <p class='other text-p'>Ou entre com...</p>
+  <input type='image' id='btn-google' class='btn-google' src='google.png'>
+  <p class='register text-p'>Não tem uma conta? <a href='/#record'>Cadastre-se</a></p>
 </form>
 `;
   const botaoLogin = container.querySelector('#login');
@@ -33,6 +33,6 @@ export const login = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     signIn(provider);
   });
-   return container;
+    return container;
 };
 
