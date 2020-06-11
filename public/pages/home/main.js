f6fe8f96362d9fc3458c66ec7289c912cde09b7d
Aline <alinesanqueza@gmail.com>
2020-06-10 22:08:31 -0300

    publicações

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,38 +1,37 @@
 // Aqui serão criados os eventos de Manipulação de DOM e templates
-import { greeting } from './data.js';
+import { createPost, readPosts } from './data.js';
 
 export const home = () => {
   const container = document.createElement('div');
 
 
   container.innerHTML = `
-    <div class="btn-back">
-    </div>
+    <div class="btn-back"></div>
     <div>
-    <img class="wave" src="imagens/perfil-avatar.png">
+      <img class="wave" src="imagens/perfil-avatar.png">
     </div>
-    <div class = 'container'>
-    <input id='post' class='re-post' type='text'>
-      <button id='photo' class='btn-social'>Photo</button>
-      <button id='send-post' class='btn-social'>Compartilhar</button>
-    </div>
-    <div class='all-posts'>
+    <form>
       <input id='post' class='post' type='text'>
-      <button id='photo' class='btn-create'>Curtida</button>
-      <button id='send-post' class='btn-create'>Compartilhar</button>
-    </div>
-    <div id='all-posts'></div>
+      <button id='send-post'>Compartilhar</button>
+      <button id='photo' class='photo'>Photo</button>
+    </form>
+    <div class='all-posts'></div>
+    <!--<form>
+      <input id='posts' class='posts' type='text'>
+      <button id='photo' class='photo'>Curtida</button>
+      </form>-->
   `;
 
-  const name = container.querySelector('#post');
-  const repost = container.querySelector('#re-post');
-  const greetingBtn = container.querySelector('#send-post');
-  const greetingMessage = container.querySelector('#all-posts');
+  const post = container.querySelector('#post');
+  const sendBtn = container.querySelector('#send-post');
+  const allPosts= container.querySelector('#all-posts');
 
-  greetingBtn.addEventListener('click', (event) => {
+  sendBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    greetingMessage.innerHTML = greeting(name.value);
+    createPost(post.value);
   });
-
+  const postTemplate = (array) =>{
+    array.map(post => allPosts.innerHTML = `<p>${post.text}</p>`)
+  }
   return container;
 };
--- public/pages/login/data.js
+++ public/pages/login/data.js
@@ -1,6 +1,7 @@
 export const welcome = email => `Bem-vindo ${email}! Que bom ver você aqui!`;
 export const errorMessage = message => `Falha: ${message}`;
 export const authEmailAndPassword = (email, password) => {
+
   firebase
     .auth()
     .signInWithEmailAndPassword(email.value, password.value)
@@ -19,6 +20,7 @@ export const signIn = (provider) => {
       console.log(result);
       const token = result.credential.accessToken;
       alert(`${welcome(inputEmail.value)}`);
+      //window.location='#home';
     })
     .catch((error) => {
       console.log(error);
