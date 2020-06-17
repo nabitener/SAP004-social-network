5e9ccee4c739019b0af41d39ceef2649d02ecdea
nabitener <na_bitener@hotmail.com>
2020-06-17 17:18:39 -0300

    Menu hamburger e delete e edit restrito

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,51 +1,106 @@
-import { createPost, readPosts, deletePost, editAndSavePost, likePosts } from './data.js';
+import {
+  createPost,
+  readPosts,
+  deletePost,
+  editAndSavePost,
+  likePosts,
+} from './data.js';
 
 export const home = () => {
-
   const container = document.createElement('div');
   container.classList.add('div-home');
 
-  container.innerHTML = `  
-  <!--<div class='out'>
-    <input type='image' src='imagens/sign-out.png' class='signout' id='sign-out>
-  </div>-->
-  <div class="btn-back"></div>
-    <div>
-      <img class="wave" src="imagens/perfil-avatar.png">
+  container.innerHTML = ` 
+  <div class='menu'> 
+  <input type='checkbox' id='check' class='check'>
+  <label for='check' class='label-icone'>
+    <img src='imagens/icone.png' alt='icone-menu'>
+  </label >
+  <nav class='nav'>
+  <ul class='ul'>
+    <li class='li-link'><a class='link'>Perfil</a></li>
+    <li class='li-link'><a class='link'>Logout</a></li>
+  </ul>
+  </nav>
+  <p class='app-name-home'>Travel Time</p>
+  </div>
+  <form method='post'>
+    <div id='input-post' class='input-post'>
+      <img class='wave' src='imagens/perfil-avatar.png'>
+      <input id='post' class='post' type='text' placeholder='Para onde vamos?'>
     </div>
-    <form>
-      <div id='write-post' class='write-post'></div>
-        <div id='input-post' class='input-post'>
-        <input id='post' class='post' type='text' placeholder='Para onde vamos?'>
+    <div id='container-private' class='container-private'>
+      <div id='btn-post' class='btn-post'>
+        <button id='send-post' class='send-post icon-post'>✈️</button>
+        <button id='photo' class='photo icon-post'>📸</button>
       </div>
-    <div id='container-private' class='container-private'></div>
-    <div id='btn-post' class='btn-post'>
-      <button id='send-post' class='send-post icon-post'>✈️</button>
-      <button id='photo' class='photo icon-post'>📸</button>
-    </div> 
-    <select id='input-private' class='input-private' name='input-private'>
-    <option id='public' class'public'>Público</option> 
-    <option id='private' class='private' selected>Privado</option>
-    </select>
-    </div>  
-      <div id='all-posts' class='all-posts'></div>
-    </form>
+      <select id='input-private' class='input-private' name='input-private'>
+        <option id='public' class'public'>Público</option>
+        <option id='private' class='private' selected>Privado</option>
+      </select>
+    </div>
+    <div id='all-posts' class='all-posts'></div>
+  </form>
   `;
-  
+
   const post = container.querySelector('#post');
   const sendBtn = container.querySelector('#send-post');
   const allPosts = container.querySelector('#all-posts');
   const privacyPost = container.querySelector('#input-private');
+  const menu = container.querySelector('#icon');
 
   sendBtn.addEventListener('click', (event) => {
     event.preventDefault();
     createPost(post.value, privacyPost.value);
     post.value = '';
     allPosts.innerHTML = '';
-    readPosts(postTemplate);
+    readPosts(postTemplate, postTemplateUser);
   });
 
   const postTemplate = (post) => {
+    const now = new Date();
+    const spaceTemplate = document.createElement('div');
+    spaceTemplate.innerHTML = `
+    
+    <div id='div-post' class='div-post'>
+    <div id='container-name' class='container-name'>
+    <div id='div-name' class='div-name'>${post.data().name}</div>
+    </div>
+    <textarea id='text-post' data-id='${post.id}' class='text-post' disabled>${
+      post.data().text
+    }</textarea>
+    <div id='div-container-btn' class='div-container-btn'>
+    <div id='div-btn' class='div-btn'>
+    <button id='curtida' data-id='${post.id}' class='curtida icon-post'>❤️${
+      post.data().likes
+    }</button>
+    <button id='comentar' class='comentar icon-post'>💬${
+      post.data().coments
+    }</button>
+    </div>
+    <div id='div-date' class=div-date>
+    <p id='privaty' class='privaty'>${post.data().privacy}</p>
+    <p id='date' class='date'>${now.getDate()}/${now.getMonth()}/${now.getFullYear()}</p>
+    </div>
+    </div>
+    </div>
+    `;
+
+    allPosts.appendChild(spaceTemplate);
+
+    const btnLikes = spaceTemplate.querySelector(
+      `#curtida[data-id='${post.id}']`
+    );
+
+    btnLikes.addEventListener('click', () => {
+      const id = btnLikes.dataset.id;
+      likePosts(id, post.data().likes);
+      allPosts.innerHTML = '';
+      readPosts(postTemplate, postTemplateUser);
+    });
+  };
+
+  const postTemplateUser = (post) => {
     const now = new Date();
     const spaceTemplate = document.createElement('div');
     spaceTemplate.innerHTML = `
@@ -59,15 +114,15 @@ export const home = () => {
     <button id='delete' data-id='${post.id}' class='delete'>❌</button>
     </div>
     </div>
-    <textarea id='text-post' data-id='${post.id}' class='text-post' disabled>${
-      post.data().text
-    }</textarea>
+    <textarea id='text-post-user' data-id='${
+      post.id
+    }' class='text-post' disabled>${post.data().text}</textarea>
     <div id='div-container-btn' class='div-container-btn'>
     <div id='div-btn' class='div-btn'>
-    <button id='curtida' class='curtida icon-post'>❤️${
-      post.data().likes
-    }</button>
-    <button id='comentar' class='comentar icon-post'>💬${
+    <button id='curtida-user' data-id='${
+      post.id
+    }' class='curtida icon-post'>❤️${post.data().likes}</button>
+    <button id='comentar-user' class='comentar icon-post'>💬${
       post.data().coments
     }</button>
     </div>
@@ -88,21 +143,23 @@ export const home = () => {
     const btnSave = spaceTemplate.querySelector('#save');
     const btnEdit = spaceTemplate.querySelector('#edit');
     const editText = spaceTemplate.querySelector(
-      `#text-post[data-id='${post.id}']`
+      `#text-post-user[data-id='${post.id}']`
     );
     const selectPrivate = spaceTemplate.querySelector('#private');
     const btnDelete = spaceTemplate.querySelector(
       `#delete[data-id='${post.id}']`
     );
-    const BtnCurtida = spaceTemplate.querySelector('#curtida');
-    const BtnComentar = spaceTemplate.querySelector('#comentar');
+    const btnLikes = spaceTemplate.querySelector(
+      `#curtida-user[data-id='${post.id}']`
+    );
+    const btnComentar = spaceTemplate.querySelector('#comentar-user');
 
     btnDelete.addEventListener('click', (event) => {
       event.preventDefault();
       const id = btnDelete.dataset.id;
-      deletePost(id, validarDelete);
+      deletePost(id);
       allPosts.innerHTML = '';
-      readPosts(postTemplate);
+      readPosts(postTemplate, postTemplateUser);
     });
     btnEdit.addEventListener('click', (event) => {
       event.preventDefault();
@@ -114,62 +171,35 @@ export const home = () => {
       savePost();
     });
 
-    const validarDelete = () => {
-      if (post.data().user_id !== firebase.auth().currentUser.uid) {
-        btnDelete.style.display = 'none';
-      }
-    };
-
     const editPost = () => {
       editText.disabled = false;
       editText.style.color = 'rgba(14, 60, 89, 1)';
       editText.style.background = 'white';
       btnSave.style.display = 'inline-block';
-      BtnCurtida.style.display = 'none';
-      BtnComentar.style.display = 'none';
+      btnLikes.style.display = 'none';
+      btnComentar.style.display = 'none';
       selectPrivate.style.display = 'inline-block';
     };
 
     const savePost = () => {
       editText.disabled = true;
-      editText.style.color = 'white';
+      editText.style.color = 'wheat';
       editText.style.background = 'rgba(191, 87, 26, 1)';
       btnSave.style.display = 'none';
-      BtnCurtida.style.display = '';
-      BtnComentar.style.display = '';
+      btnLikes.style.display = '';
+      btnComentar.style.display = '';
       selectPrivate.style.display = 'none';
       const id = editText.dataset.id;
 
       editAndSavePost(id, editText.value, selectPrivate.value);
       allPosts.innerHTML = '';
-      readPosts(postTemplate);
+      readPosts(postTemplate, postTemplateUser);
     };
+  };
 
-    // console.log(post.id)
-    const btnLikes =  spaceTemplate.querySelector(`button[data-id='${post.id}']`);
-    // console.log(btnLikes);
-    btnLikes.addEventListener( 'click' , () => {
-      // console.log("like")
-      const id = btnLikes.dataset.id
-      console.log(id);
-      // const id = post.id
-      likePosts(id,post.data().likes);
-      allPosts.innerHTML = '';
-      readPosts(postTemplate);
-    });
-    };
-
-    
-
-  readPosts(postTemplate);
+  readPosts(postTemplate, postTemplateUser);
 
   return container;
 };
 
 
-/*const validarEdit = () => {
-      if (post.data().user_id !== firebase.auth().currentUser.uid) {
-        edit.style.display = 'none';
-      }
-    }
-*/
